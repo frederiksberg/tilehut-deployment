d1fad84be0adbf6464ba6873d0faa900dd37b74f
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-06-14 10:52:06 +0200

    Fixed typo in init.sh

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@
 
 make deploy
 
-docker exec tilehut_deployment_nginx_1 certbot certonly --nginx -d th.frb-data.dk --non-interactive --agree-tos -m gis@frederiksberg.dk
+docker exec tilehut-deployment_nginx_1 certbot certonly --nginx -d th.frb-data.dk --non-interactive --agree-tos -m gis@frederiksberg.dk
 
-docker exec tilehut_deployment_nginx_1 sh -c 'echo ''certbot renew --post-hook "systemctl reload nginx"'' > /etc/cron.daily'
+docker exec tilehut-deployment_nginx_1 sh -c 'echo ''certbot renew --post-hook "systemctl reload nginx"'' > /etc/cron.daily'
 
 make kill
