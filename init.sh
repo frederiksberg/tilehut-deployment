7da7eaab7c0aa339fa6b38185cba4372609cfd4c
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-06-14 11:30:10 +0200

    Changed init behaviour

--- init.sh
+++ init.sh
@@ -3,6 +3,11 @@
 # Script to be run on very first setup.
 # Replace dummy keys with real keys before beginning this
 
+# Create host_keys
+mkdir -p host_keys
+ssh-keygen -t ed25519 -f host_keys/ssh_host_ed25519_key < /dev/null
+ssh-keygen -t rsa -b 4096 -f host_keys/ssh_host_rsa_key < /dev/null
+
 make deploy
 
 docker exec tilehut-deployment_nginx_1 certbot certonly --nginx -d th.frb-data.dk --non-interactive --agree-tos -m gis@frederiksberg.dk
