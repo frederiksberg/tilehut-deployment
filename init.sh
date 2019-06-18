7002530395fbc4084010203a80809c23d3ebaff3
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-06-18 13:22:39 +0200

    Final tweaks to init

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@
 # Replace dummy keys with real keys before beginning this
 
 GREEN='\u001b[32;1m'
+YELLOW='\u001b[33;1m'
 RESET='\u001b[0m'
 
 echo -e "$GREEN--> Building host keys$RESET"
@@ -13,16 +14,17 @@ ssh-keygen -t ed25519 -f host_keys/ssh_host_ed25519_key -N '' < /dev/null 2>&1 >
 ssh-keygen -t rsa -b 4096 -f host_keys/ssh_host_rsa_key -N '' < /dev/null 2>&1 >/dev/null
 
 echo -e "$GREEN--> Deploying web server$RESET"
-make deploy
+make deploy 2>&1 >/dev/null
 
-echo -e "$GREEN--> Waiting 10s for nginx to gen DH keys$RESET"
-sleep 10 # Sleep for 10 seconds. If this is not enough try again.
+echo -e "$GREEN--> Waiting 5s for nginx to initialize$RESET"
+sleep 5 # Sleep for 5 seconds. If this is not enough try again.
 
 echo -e "$GREEN--> Getting certificate$RESET"
-docker exec tilehut-deployment_nginx_1 certbot --nginx -d th.frb-data.dk -nq --agree-tos --no-eff-email -m gis@frederiksberg.dk --staging
+echo -e "$YELLOW--> An invalid PID number error can occur here. This is fine$RESET"
+docker exec tilehut-deployment_nginx_1 certbot --nginx -d th.frb-data.dk -nq --agree-tos --no-eff-email -m gis@frederiksberg.dk
 
 echo -e "$GREEN--> Setting up auto renewal$RESET"
 docker exec tilehut-deployment_nginx_1 sh -c 'echo ''certbot renew --post-hook "systemctl reload nginx"'' > /etc/cron.daily'
 
 echo -e "$GREEN--> Bringing down web server$RESET"
-make kill
+make kill 2>&1 >/dev/null
